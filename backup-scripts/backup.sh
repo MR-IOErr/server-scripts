43155d507fd800a6a9ddf371d5881cc83787a5c9
Parsa <110420882+parsaradnezhad@users.noreply.github.com>
2024-07-09 19:01:53 +0330

    Update backup.sh

--- backup-scripts/backup.sh
+++ backup-scripts/backup.sh
@@ -1,113 +1,77 @@
 #!/bin/bash
 
-# Function to display usage and exit
-usage() {
-  echo "Usage: $0 -h <mysql-host> -P <mysql-port> -u <mysql-username> -p <mysql-password> -d <mysql-database> -b1 <s3_bucket_mysql> -b2 <s3_bucket_nginx> -b3 <s3_bucket_www> -k <encryption_password>"
-  exit 1
+# Load environment variables from .env file
+if [ -f .env ]; then
+  export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+fi
+
+# Function to backup MySQL database
+backup_mysql() {
+  local db_name=$1
+  local db_user=$2
+  local db_password=$3
+  local bucket_name=$4
+  local encryption_password=$5
+  local aws_access_key_id=$6
+  local aws_secret_access_key=$7
+  local timestamp=$(date +%Y%m%d)
+  local backup_filename="${db_name}_backup_$timestamp"
+
+  echo "Starting MySQL backup for $db_name..."
+  mysqldump -h $MYSQL_HOST -u $db_user -p$db_password $db_name > /tmp/$backup_filename.sql
+  tar -cf /tmp/$backup_filename.tar -C /tmp $backup_filename.sql
+  xz /tmp/$backup_filename.tar
+  openssl enc -aes-256-cbc -salt -in /tmp/$backup_filename.tar.xz -out /tmp/$backup_filename.enc -k $encryption_password
+
+  aws configure set aws_access_key_id $aws_access_key_id
+  aws configure set aws_secret_access_key $aws_secret_access_key
+
+  aws s3 cp --endpoint-url https://api-s3.nxbo.ir /tmp/$backup_filename.enc s3://$bucket_name/$backup_filename.enc
+
+  rm /tmp/$backup_filename.sql /tmp/$backup_filename.tar.xz /tmp/$backup_filename.enc
+  echo "MySQL backup for $db_name completed."
 }
 
-# Parse input arguments
-while getopts ":h:P:u:p:d:b1:b2:b3:k:" opt; do
-  case $opt in
-    h) DB_HOST="$OPTARG"
-    ;;
-    P) DB_PORT="$OPTARG"
-    ;;
-    u) DB_USER="$OPTARG"
-    ;;
-    p) DB_PASS="$OPTARG"
-    ;;
-    d) DB_NAME="$OPTARG"
-    ;;
-    b1) S3_BUCKET_MYSQL="$OPTARG"
-    ;;
-    b2) S3_BUCKET_NGINX="$OPTARG"
-    ;;
-    b3) S3_BUCKET_WWW="$OPTARG"
-    ;;
-    k) ENCRYPTION_PASSWORD="$OPTARG"
-    ;;
-    *) usage
-    ;;
-  esac
-done
+# Function to backup directories
+backup_directory() {
+  local src_dir=$1
+  local bucket_name=$2
+  local encryption_password=$3
+  local aws_access_key_id=$4
+  local aws_secret_access_key=$5
+  local prefix=$6
+  local dir_name=$(basename $src_dir)
+  local timestamp=$(date +%Y%m%d)
+  local backup_filename="${prefix}_${dir_name}_backup_$timestamp"
 
-# Check if all required variables are set
-if [ -z "$DB_HOST" ] || [ -z "$DB_PORT" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASS" ] || [ -z "$DB_NAME" ] || [ -z "$S3_BUCKET_MYSQL" ] || [ -z "$S3_BUCKET_NGINX" ] || [ -z "$S3_BUCKET_WWW" ] || [ -z "$ENCRYPTION_PASSWORD" ]; then
-    echo "Missing required arguments."
-    usage
-fi
+  echo "Starting backup of $src_dir..."
+  tar -cf /tmp/$backup_filename.tar -C $(dirname $src_dir) $(basename $src_dir)
+  xz /tmp/$backup_filename.tar
+  openssl enc -aes-256-cbc -salt -in /tmp/$backup_filename.tar.xz -out /tmp/$backup_filename.enc -k $encryption_password
 
-# Perform MySQL backup
-DATE=$(date +"%Y%m%d")
-MYSQL_BACKUP_FILE="/tmp/${DB_NAME}_mysql_backup_$DATE.sql"
+  aws configure set aws_access_key_id $aws_access_key_id
+  aws configure set aws_secret_access_key $aws_secret_access_key
 
-# Perform the MySQL backup using mysqldump
-mysqldump -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > "$MYSQL_BACKUP_FILE"
+  aws s3 cp --endpoint-url https://api-s3.nxbo.ir /tmp/$backup_filename.enc s3://$bucket_name/$backup_filename.enc
 
-# Check if the backup was successful
-if [ $? -ne 0 ]; then
-  echo "MySQL dump failed. Exiting."
-  exit 1
-fi
+  rm /tmp/$backup_filename.tar.xz /tmp/$backup_filename.enc
+  echo "Backup of $src_dir completed."
+}
 
+# MySQL backups
 
-sync "$MYSQL_BACKUP_FILE"
+backup_mysql $HELP_DB_NAME $HELP_DB_USER $HELP_DB_PASSWORD $HELP_BUCKET $HELP_ENCRYPTION_PASSWORD $HELP_AWS_ACCESS_KEY_ID $HELP_AWS_SECRET_ACCESS_KEY
+backup_mysql $MAG_DB_NAME $MAG_DB_USER $MAG_DB_PASSWORD $MAG_BUCKET $MAG_ENCRYPTION_PASSWORD $MAG_AWS_ACCESS_KEY_ID $MAG_AWS_SECRET_ACCESS_KEY
+backup_mysql $ACADEMY_DB_NAME $ACADEMY_DB_USER $ACADEMY_DB_PASSWORD $ACADEMY_BUCKET $ACADEMY_ENCRYPTION_PASSWORD $ACADEMY_AWS_ACCESS_KEY_ID $ACADEMY_AWS_SECRET_ACCESS_KEY
 
+# Nginx backups
 
-# Tar MySQL backup
-tar czf "${MYSQL_BACKUP_FILE}.tar.gz" "$MYSQL_BACKUP_FILE"
+#backup_directory "/etc/nginx/sites-enabled/academy" $NGINX_ACADEMY_BUCKET $NGINX_ENCRYPTION_PASSWORD $NGINX_ACADEMY_AWS_ACCESS_KEY_ID $NGINX_ACADEMY_AWS_SECRET_ACCESS_KEY "nginx"
+#backup_directory "/etc/nginx/sites-enabled/help" $NGINX_HELP_BUCKET $NGINX_ENCRYPTION_PASSWORD $NGINX_HELP_AWS_ACCESS_KEY_ID $NGINX_HELP_AWS_SECRET_ACCESS_KEY "nginx"
 
-# Encrypt MySQL backup using OpenSSL with the provided encryption password
-openssl enc -aes-256-cbc -salt -in "${MYSQL_BACKUP_FILE}.tar.gz" -out "${MYSQL_BACKUP_FILE}.tar.gz.enc" -k "$ENCRYPTION_PASSWORD"
+# HTML backups
 
-# Upload MySQL encrypted backup file to S3 bucket for MySQL
-aws s3 cp --endpoint-url https://api-s3.nxbo.ir "${MYSQL_BACKUP_FILE}.tar.gz.enc" "s3://$S3_BUCKET_MYSQL/${DB_NAME}_mysql_backup_$DATE.sql.tar.gz.enc"
+#backup_directory "/var/www/html/help" $HTML_HELP_BUCKET $HTML_ENCRYPTION_PASSWORD $HTML_HELP_AWS_ACCESS_KEY_ID $HTML_HELP_AWS_SECRET_ACCESS_KEY "html"
+#backup_directory "/var/www/html/mag" $HTML_MAG_BUCKET $HTML_ENCRYPTION_PASSWORD $HTML_MAG_AWS_ACCESS_KEY_ID $HTML_MAG_AWS_SECRET_ACCESS_KEY "html"
 
-# Check if the upload was successful
-if [ $? -eq 0 ]; then
-  echo "Encrypted MySQL backup uploaded to S3 successfully: ${DB_NAME}_mysql_backup_$DATE.sql.tar.gz.enc"
-else
-  echo "Upload of MySQL backup to S3 failed!"
-fi
-
-# Backup /var/www/ directory
-WWW_BACKUP_DIR="/tmp/www_backup"
-mkdir -p "$WWW_BACKUP_DIR"
-tar czf "${WWW_BACKUP_DIR}/${HOSTNAME}_www_backup_$DATE.tar.gz" -C /var/www .
-
-# Encrypt /var/www/ backup using OpenSSL with the provided encryption password
-openssl enc -aes-256-cbc -salt -in "${WWW_BACKUP_DIR}/${HOSTNAME}_www_backup_$DATE.tar.gz" -out "${WWW_BACKUP_DIR}/${HOSTNAME}_www_backup_$DATE.tar.gz.enc" -k "$ENCRYPTION_PASSWORD"
-
-# Upload /var/www/ encrypted backup file to S3 bucket for WWW
-aws s3 cp --endpoint-url https://api-s3.nxbo.ir "${WWW_BACKUP_DIR}/${HOSTNAME}_www_backup_$DATE.tar.gz.enc" "s3://$S3_BUCKET_WWW/${HOSTNAME}_www_backup_$DATE.tar.gz.enc"
-
-# Check if the upload was successful
-if [ $? -eq 0 ]; then
-  echo "Encrypted /var/www/ backup uploaded to S3 successfully: ${HOSTNAME}_www_backup_$DATE.tar.gz.enc"
-else
-  echo "Upload of /var/www/ backup to S3 failed!"
-fi
-
-# Backup /etc/nginx/ directory
-NGINX_BACKUP_DIR="/tmp/nginx_backup"
-mkdir -p "$NGINX_BACKUP_DIR"
-tar czf "${NGINX_BACKUP_DIR}/${HOSTNAME}_nginx_backup_$DATE.tar.gz" -C /etc/nginx .
-
-# Encrypt /etc/nginx/ backup using OpenSSL with the provided encryption password
-openssl enc -aes-256-cbc -salt -in "${NGINX_BACKUP_DIR}/${HOSTNAME}_nginx_backup_$DATE.tar.gz" -out "${NGINX_BACKUP_DIR}/${HOSTNAME}_nginx_backup_$DATE.tar.gz.enc" -k "$ENCRYPTION_PASSWORD"
-
-# Upload /etc/nginx/ encrypted backup file to S3 bucket for NGINX
-aws s3 cp --endpoint-url https://api-s3.nxbo.ir "${NGINX_BACKUP_DIR}/${HOSTNAME}_nginx_backup_$DATE.tar.gz.enc" "s3://$S3_BUCKET_NGINX/${HOSTNAME}_nginx_backup_$DATE.tar.gz.enc"
-
-# Check if the upload was successful
-if [ $? -eq 0 ]; then
-  echo "Encrypted /etc/nginx/ backup uploaded to S3 successfully: ${HOSTNAME}_nginx_backup_$DATE.tar.gz.enc"
-else
-  echo "Upload of /etc/nginx/ backup to S3 failed!"
-fi
-
-# Clean up temporary backup files
-rm -rf "$WWW_BACKUP_DIR" "$NGINX_BACKUP_DIR" "${MYSQL_BACKUP_FILE}.tar.gz" "${MYSQL_BACKUP_FILE}.tar.gz.enc" "${WWW_BACKUP_DIR}/${HOSTNAME}_www_backup_$DATE.tar.gz" "${WWW_BACKUP_DIR}/${HOSTNAME}_www_backup_$DATE.tar.gz.enc" "${NGINX_BACKUP_DIR}/${HOSTNAME}_nginx_backup_$DATE.tar.gz" "${NGINX_BACKUP_DIR}/${HOSTNAME}_nginx_backup_$DATE.tar.gz.enc"
-
-echo "Temporary backup files cleaned up."
+echo "All backups completed."
