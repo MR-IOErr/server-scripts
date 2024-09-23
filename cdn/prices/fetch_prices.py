c5cc944931ab8a5124836c401fc30581cd0cfd4d
SeyedAli Aletaha <seyedalialetaha7@gmail.com>
2024-09-23 15:53:32 +0330

    NewCoins

--- cdn/prices/fetch_prices.py
+++ cdn/prices/fetch_prices.py
@@ -27,7 +27,7 @@ TRACKED_COINS = [
     'WLD', 'OMG', 'ILV', 'RDNT', 'JST', 'ELF', 'NOT',
     'WOO', 'FET', 'RSR', 'AEVO', 'PEPE', 'OM', 'BICO', 'GALA', 'GAL',
     'AGLD', 'ETHFI', 'RENDER', 'SKL', 'TRB', 'AGIX', 'G', 'ONDO',
-    'DOGS', 'CATI',
+    'DOGS', 'CATI', 'HMSTR',
 ]
 
 
